66609ae0fadc4ef803f8811aca9e934f5a3db13c
kowalix007 <kowalix007@gmail.com>
2020-01-31 21:49:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,9 @@
 require 'pry'
 class Artist
- extend Memorable
+   extend 
+  Memorable::ClassMethods
+  include
+  Memorable::InstanceMethods
  include Paramable
   attr_accessor :name
   attr_reader :songs
