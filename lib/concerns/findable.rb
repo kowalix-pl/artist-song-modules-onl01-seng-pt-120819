3fb9f373fd9a9bc97fa15364e046b0f389dbd57d
kowalix007 <kowalix007@gmail.com>
2020-01-31 22:08:54 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -4,8 +4,8 @@ module Findable
     artists.detect{|a| a.name == name}
   end
  
- def find_by_name(name)
-    self.all.detect{|a| a.name == name}
+ def self.find_by_name(name)
+    songs.detect{|a| a.name == name}
   end
 
 end 
